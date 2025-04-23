48f8a17bf586f89fa9dfb0661dc04421761ceab1
jllee000 <jllee000@naver.com>
2025-04-24 00:28:50 +0900

    feat : 입력 로직

--- week2/week2-HW/index.js
+++ week2/week2-HW/index.js
@@ -59,3 +59,15 @@ liElements.forEach((li) => {
     this.classList.add("active");
   });
 });
+
+// 입력 로직
+const searchBtn = document.querySelector(".search-bar .search-btn");
+const searchTxt = document.querySelector(".search-bar .search-input");
+const searchSelect = document.querySelector(".search-bar .search-select");
+searchBtn.addEventListener("click", function () {
+  if (searchSelect.value == "score-default" || searchTxt.value === "") {
+    alert("입력조건을 확인해주세요 (할 일 입력 및 중요도 지정)");
+  } else {
+    console.log(searchTxt.value);
+  }
+});
