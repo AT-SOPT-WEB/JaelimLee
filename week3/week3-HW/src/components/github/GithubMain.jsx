0fe3ea34f5ee163566b3593922daf259ca5b462d
jllee000 <jllee000@naver.com>
2025-05-06 14:54:15 +0900

    feat : git검색페이지 최근검색어 컴포넌트화

--- week3/week3-HW/src/components/github/GithubMain.jsx
+++ week3/week3-HW/src/components/github/GithubMain.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { css } from "@emotion/react";
 import GithubInput from "./GithubInput";
+import RecentSearch from "./RecentSearch";
 /** @jsxImportSource @emotion/react */
 
 const mainStyle = css`
@@ -59,39 +60,6 @@ const gitBoxCloseStyle = css`
   cursor: pointer;
 `;
 
-const recentSearchStyle = css`
-  display: flex;
-  gap: 10px;
-  width: 80%;
-  overflow-x: scroll;
-  &::-webkit-scrollbar {
-    display: none; /* 스크롤바 숨기기 */
-  }
-`;
-
-const recentSearchItemStyle = css`
-  display: flex;
-  padding: 12px;
-  justify-content: space-between;
-  gap: 10px;
-  align-items: center;
-  background-color: rgba(187, 222, 251, 0.49);
-  border-radius: 17px;
-  cursor: pointer;
-`;
-
-const recentSearchTextStyle = css`
-  font-size: 1rem;
-  color: rgb(1, 36, 65);
-`;
-
-const githubCloseStyle = css`
-  padding: 5px 10px;
-  border-radius: 999px;
-  cursor: pointer;
-  color: white;
-  background-color: black;
-`;
 const GithubMain = () => {
   const [userInfo, setUserInfo] = useState({ status: "idle", data: null });
   const [githubUser, setGithubUser] = useState("");
@@ -164,24 +132,11 @@ const GithubMain = () => {
 
       {/* 최근 검색어 */}
       {recentSearches.length > 0 && (
-        <div css={recentSearchStyle}>
-          {recentSearches.reverse().map((searchTerm, index) => (
-            <div key={index} css={recentSearchItemStyle}>
-              <span
-                css={recentSearchTextStyle}
-                onClick={() => handleRecentSearchClick(searchTerm)}
-              >
-                {searchTerm}
-              </span>
-              <button
-                css={githubCloseStyle}
-                onClick={() => handleDeleteSearch(searchTerm)}
-              >
-                x
-              </button>
-            </div>
-          ))}
-        </div>
+        <RecentSearch
+          recentSearches={recentSearches}
+          onSearchClick={handleRecentSearchClick}
+          onDeleteClick={handleDeleteSearch}
+        />
       )}
 
       {userInfo.status === "resolved" && showGitBox && (
